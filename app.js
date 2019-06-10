581d5ebd3fd39eb5cb44127979820c513ae2ff90
matthunterking <matthew85king@gmail.com>
2019-06-10 12:28:54 +0100

    Added basic functionality

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("loaded");
+  const $startButton = document.querySelector("#startButton");
+  const $timerDisplay = document.querySelector("#timerDisplay");
+  const $slideNumberDisplay = document.querySelector("#slideNumberDisplay");
+
+  let slideNumber = 1;
+
+  const startClock = () => {
+    let timer = 21;
+    const timerInterval = setInterval(function() {
+      timer--;
+      $timerDisplay.innerHTML = timer;
+      if (timer === 0) {
+        clearInterval(timerInterval);
+        updateSlideNumber();
+      }
+    }, 10);
+  };
+
+  const updateSlideNumber = () => {
+    slideNumber++;
+    $slideNumberDisplay.innerHTML = slideNumber;
+    if (slideNumber < 21) startClock($timerDisplay, $slideNumberDisplay);
+  };
+  $startButton.addEventListener("click", e => {
+    e.preventDefault();
+    startClock();
+  });
 });
