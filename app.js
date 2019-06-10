07f7ad55d2834017e113e66ef5d3fc9b51f8cf54
matthunterking <matthew85king@gmail.com>
2019-06-10 13:08:54 +0100

    Added sounds and reset button

--- app.js
+++ app.js
@@ -2,28 +2,58 @@ window.addEventListener("DOMContentLoaded", () => {
   const $startButton = document.querySelector("#startButton");
   const $timerDisplay = document.querySelector("#timerDisplay");
   const $slideNumberDisplay = document.querySelector("#slideNumberDisplay");
+  const $body = document.querySelector("body");
+  const $audio = document.querySelector("audio");
 
   let slideNumber = 1;
+  let isRunning = false;
+  let timerInterval;
 
   const startClock = () => {
     let timer = 21;
-    const timerInterval = setInterval(function() {
+    $timerDisplay.innerHTML = timer;
+    timerInterval = setInterval(function() {
       timer--;
       $timerDisplay.innerHTML = timer;
       if (timer === 0) {
         clearInterval(timerInterval);
         updateSlideNumber();
       }
-    }, 10);
+    }, 1000);
+  };
+
+  const reset = () => {
+    $startButton.innerHTML = "Start";
+    slideNumber = 1;
+    $slideNumberDisplay.innerHTML = slideNumber;
+    clearInterval(timerInterval);
+    timer = 21;
+    $timerDisplay.innerHTML = timer;
+    $body.style.backgroundColor = "rgb(152, 216, 236)";
+    $audio.src = "./ding.mp3";
+    isRunning = false;
   };
 
   const updateSlideNumber = () => {
     slideNumber++;
     $slideNumberDisplay.innerHTML = slideNumber;
-    if (slideNumber < 21) startClock($timerDisplay, $slideNumberDisplay);
+    $audio.play();
+    if (slideNumber < 21) {
+      startClock($timerDisplay, $slideNumberDisplay);
+    } else {
+      $body.style.backgroundColor = "red";
+      $audio.src = "./gong.mp3";
+      $audio.play();
+    }
   };
   $startButton.addEventListener("click", e => {
     e.preventDefault();
-    startClock();
+    if (!isRunning) {
+      isRunning = true;
+      $startButton.innerHTML = "Stop and Reset";
+      startClock();
+    } else {
+      reset();
+    }
   });
 });
